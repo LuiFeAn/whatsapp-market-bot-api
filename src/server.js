b22901692ab5c839dc3fc3666483312e6a1d0198
LuiFeAn <luisfernandogvv@gmail.com>
2023-07-19 15:27:45 -0300

    feat: Dando início a importação e criação do servidor web

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const env = require("dotenv");
+
+env.config();
+
+const PORT_ = process.env.SERVER_PORT || 3004
+
+app.listen(PORT_,function(){
+    console.log(`O servidor HTTP foi iniciado na prota ${PORT_} e está pronto para receber requisições`);
+});
