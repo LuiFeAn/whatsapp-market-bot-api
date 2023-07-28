732849d1425458e6c89e400e00c5c7975ab8fbdb
LuiFeAn <luisfernandogvv@gmail.com>
2023-07-28 20:26:27 -0300

    feat: Criando repositório de pedidos, além dos métodos create/update

--- src/repositories/demandRepository.js
+++ src/repositories/demandRepository.js
@@ -1,8 +1,22 @@
-
+const query = require("../database/mysql-async");
 
 class DemandRepository {
 
     create(cart_id){
+
+        return query('ECONOBOT',{
+            query:'INSERT INTO pedidos VALUES (NULL,?)',
+            values:[cart_id]
+        });
+
+    }
+    
+    delete(cart_id){
+
+        return query('ECONOBOT',{
+            query:'DELETE FROM pedidos WHERE id = ?',
+            values:[cart_id]
+        });
 
     }
 
