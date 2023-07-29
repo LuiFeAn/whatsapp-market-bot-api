66df4f7265f90734fd3231d300ac04846e5c3f37
Luis <luisfernandogvv@gmail.com>
2023-07-29 11:49:21 -0300

    feat: ATT

--- src/services/demandService.js
+++ src/services/demandService.js
@@ -1,4 +1,6 @@
 const demandRepository = require("../repositories/demandRepository");
+const clearMemoryService = require("../services/clearMemoryService");
+const bot = require("../index");
 
 class DemandService {
 
@@ -20,9 +22,44 @@ class DemandService {
 
     async partialUpdate({ id, status }){
 
+        status.toUpperCase();
+
+        const demand = await this.getDemand(id);
+
         if( status ){
 
-            await demandRepository.updateStatus(id,status);
+            if( status === 'RECUSADO' ){
+
+                clearMemoryService.clearUserLastProductAndList(demand.usuario_id);
+
+                await bot.say(demand.usuario_id,`${demand.usuario_id}, infelizmente seu pedido foi recusado.`);
+
+                await demandRepository.updateStatus(id,'PEDIDO RECUSADO');
+
+                return
+
+            }
+
+            if( status === 'APROVADO' ){
+
+                await bot.say(demand.usuario_id,`${demand.usuario_id}, Seu pedido foi aceito e já se encontra em fase de separação ! 😍.`);
+
+                await demandRepository.updateStatus(id,'EM FASE DE SEPARAÇÃO');
+
+                return
+
+            }
+
+            if( status === 'SAIU PARA ENTREGA'){
+
+            }
+
+            if( status === 'FINALIZADO' ){
+
+                
+
+            }
+    
 
         }
 
