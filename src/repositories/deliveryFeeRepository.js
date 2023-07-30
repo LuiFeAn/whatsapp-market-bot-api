dc4d6e8aa0bd802f113a184a450eaaac4c7b0aa2
LuiFeAn <luisfernandogvv@gmail.com>
2023-07-30 10:56:13 -0300

    feat: Criando repositoório de taxa de entrega e seus respectivos métodos

---
+++ src/repositories/deliveryFeeRepository.js
@@ -0,0 +1,54 @@
+const query = require('../database/mysql-async');
+
+
+class DeliveryFeeRepository {
+
+
+    findAll(){
+
+        return query('ECONOBOT',{
+            query:'SELECT * FROM taxa_entrega',
+            values:[]
+        })
+
+    }
+
+    create({ kmMaximo, kmFrete, taxa }){
+
+        return query('ECONOBOT',{
+            query:'INSERT INTO taxa_entrega VALUE(NULL,?,?,?)',
+            values:[kmMaximo,kmFrete,taxa]
+        });
+
+    }
+
+   updateMaxKm(id,maxKm){
+
+        return query('ECONOBOT',{
+            query:'UPDATE taxa_entrega SET km_maximo = ? WHERE id = ?',
+            values:[maxKm,id]
+        })
+
+   }
+
+   updateFeeKm(id,freeKm){
+
+        return query('ECONOBOT',{
+            query:'UPDATE taxa_entrega SET km_frete = ? WHERE id = ?',
+            values:[freeKm,id]
+        })
+
+   }
+
+   updateRate(id,rate){
+
+        return query('ECONOBOT',{
+            query:'UPDATE taxa_entrega SET taxa = ? WHERE id = ?',
+            values:[rate,id]
+        })
+
+   }
+
+}
+
+module.exports = new DeliveryFeeRepository();
