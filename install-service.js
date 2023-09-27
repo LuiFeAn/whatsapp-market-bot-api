c1bc3262aecb192dc28ccd2e8a36ce10c1dbabf8
LuiFeAn <luisfernandogvv@gmail.com>
2023-09-27 19:44:50 -0300

    refactor: Aplicando variáveis de ambiente ao install-service.js

--- install-service.js
+++ install-service.js
@@ -2,25 +2,27 @@ const Service = require('node-windows').Service;
 
 const path = require('path');
 
+const env = require('dotenv');
+
 const econoApiSVC = new Service({
     name:'Econobot',
     description: 'Serviço responsável pela aplicação Econobot, BOT responsável pelo atendimento ao cliente.',
     script: path.join(__dirname,'src','index.js'),
     env: [
-        { name: 'SERVER_PORT', value: '3001' },
-        { name: 'ECONOBOT_DB_USER', value: '' },
-        { name: 'ECONOBOT_DB_HOST', value: '' },
-        { name: 'ECONOBOT_DB_PASSWORD', value: '' },
-        { name: 'ECONOBOT_DB_NAME', value: '' },
-        { name: 'ECONOBOT_DB_PORT', value: '' },
-        { name: 'BOT_ENABLE', value: 'true' },
-        { name: 'ECONOCOMPRAS_DB_USER', value: '' },
-        { name: 'ECONOCOMPRAS_DB_HOST', value: '' },
-        { name: 'ECONOCOMPRAS_DB_PASSWORD', value: '' },
-        { name: 'ECONOCOMPRAS_DB_NAME', value: '' },
-        { name: 'ECONOCOMPRAS_DB_PORT', value: '' },
-        { name: 'ECONOCOMPRAS_PIX_KEY', value: '' },
-        { name: 'ECONOCOMPRAS_PIX_USER', value: '' }
+        { name: 'SERVER_PORT', value: '3005' },
+        { name: 'ECONOBOT_DB_USER', value: process.env.ECONOBOT_DB_USER },
+        { name: 'ECONOBOT_DB_HOST', value: process.env.ECONOBOT_DB_HOST  },
+        { name: 'ECONOBOT_DB_PASSWORD', value: process.env.ECONOBOT_DB_PASSWORD },
+        { name: 'ECONOBOT_DB_NAME', value: process.env.ECONOBOT_DB_NAME },
+        { name: 'ECONOBOT_DB_PORT', value: process.env.ECONOBOT_DB_PORT },
+        { name: 'BOT_ENABLE', value: process.env.BOT_ENABLE },
+        { name: 'ECONOCOMPRAS_DB_USER', value: process.env.ECONOCOMPRAS_DB_USER },
+        { name: 'ECONOCOMPRAS_DB_HOST', value: process.env.ECONOCOMPRAS_DB_HOST },
+        { name: 'ECONOCOMPRAS_DB_PASSWORD', value: process.env.ECONOCOMPRAS_DB_PASSWORD },
+        { name: 'ECONOCOMPRAS_DB_NAME', value: process.env.ECONOCOMPRAS_DB_NAME },
+        { name: 'ECONOCOMPRAS_DB_PORT', value: process.env.ECONOCOMPRAS_DB_PORT },
+        { name: 'ECONOCOMPRAS_PIX_KEY', value: process.env.ECONOCOMPRAS_PIX_KEY },
+        { name: 'ECONOCOMPRAS_PIX_USER', value: process.env.ECONOCOMPRAS_PIX_USER }
     ]
 });
 
